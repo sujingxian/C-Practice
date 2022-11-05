6a24734f578cb576ffa6f91c05d9035a361dbaa9
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2022-11-05 16:26:46 +1100

    Add files via upload
    Check how many words that user input

---
+++ DailyPractice03/DailyPractice03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DailyPractice03
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the string");
+            string a;
+            a = Console.ReadLine();
+            Console.WriteLine("the string that you input is " + a);
+
+            int Count = 0;
+            foreach (char c in a)
+            {
+                if (c == ' ')
+                {
+                    Count++;
+                }
+            }
+            int k = Count +1;
+
+            Console.WriteLine("You totally input " + k + "words");
+            
+
+        }
+    }
+}
