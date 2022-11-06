4739690ccc0626e9199d9671b83372218e6bb77a
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2022-11-06 21:53:55 +1100

    Add files via upload

---
+++ DailyPractice04/DailyPractice04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DailyPractice04
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] myNum = new string[11];
+            for(int i=0; i <=10; i++)
+            {
+                decimal num = 0;
+                Console.Write("Please enter the " + i + " number, ");
+                myNum[i] = Console.ReadLine();
+                bool isNum = decimal.TryParse(myNum[i], out num);
+                if (isNum)
+                {
+                    Console.WriteLine("This number is " + myNum[i]);
+
+                }
+                else
+                {
+                    Console.WriteLine("Please try again");
+                    i = i - 1;
+                }
+
+
+                
+            }
+            foreach(var value in myNum)
+            {
+                Console.Write(value);
+            }
+        }
+    }
+}
