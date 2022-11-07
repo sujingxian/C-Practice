5d5943f9610ea1c917da01986f2cd9e562e4e04d
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2022-11-07 22:33:16 +1100

    Add files via upload

---
+++ DailyPractice05/DailyPractice05/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DailyPractice05
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string a;
+            while (true)
+            {
+                Console.WriteLine("Please enter a string");
+                a = Console.ReadLine();
+                char[] s = a.ToCharArray();
+                if (s.Length != 5)
+                {
+                    Console.WriteLine("Please enter 5 letters");
+
+                }
+                else
+                {
+                    bool flag = true;
+                    foreach (var value in s)
+                    {
+                        if (!char.IsUpper(value))
+                        {
+                            flag = false;
+                        }
+                        
+                    }
+                    if (flag)
+                    {
+                        Console.WriteLine("Yes Upper");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter upper");
+                    }
+                }
+            }
+            
+
+            
+        }
+    }
+}
