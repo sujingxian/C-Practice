0098c5cba4989b85424b3f7b09aa2ada12dc13b9
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2022-11-09 23:13:27 +1100

    Add files via upload

---
+++ DailyPractice07/DailyPractice07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DailyPractice07
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            byte[] arr = new byte[10];
+            Random random = new Random();
+
+            random.NextBytes(arr);
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            for(int i = 0; i<10; i++)
+            {
+                Console.WriteLine("arr[{0}] = {1}", i, arr[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
