cdde4a49634bfa4232566813afba944951dc9308
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2022-11-09 11:53:43 +1100

    Add files via upload

---
+++ DailyPractice06/DailyPractice06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DailyPractice06
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[,] arr = new int[2, 3];
+            //two dimention arra
+            long sum = 0;
+            // long  -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+            Random random = new Random();
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j<3; j++)
+                {
+                    arr[i, j] = random.Next();
+                    sum += arr[i, j];
+                    Console.Write("arr[{0},{1}] = {2}\t", i, j, arr[i,j]);
+                }
+
+                Console.WriteLine("sum = {0}", sum);
+                sum = 0;
+                
+                
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
