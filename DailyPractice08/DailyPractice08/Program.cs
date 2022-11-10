a8597ecce00e57ab3eab7337cc7302fb9a613b23
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2022-11-10 22:04:22 +1100

    Add files via upload

---
+++ DailyPractice08/DailyPractice08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyPractice08
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 2, 38, 29, 19, 10, 9, 3, 58, 4, 1 };
+            List<int> list = arr.ToList();
+            int ans;
+            Random random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                ans = list[random.Next(arr.Length - i)];
+                list.Remove(ans);
+                Console.Write(ans + " ");
+                //list.Remove(ans);
+
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
