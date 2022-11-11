b707f1e298bb8d20c6c4deae45fafbd42e1957d2
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2022-11-11 22:29:24 +1100

    Add files via upload

---
+++ DailyPractice09/DailyPractice09/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DailyPractice09
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string uInput;
+            // Datetime structure should be 2019,12,03 or 2019-12-03 or 2019 12 3 or 2019/12/2
+            DateTime date;
+            while (true)
+            {
+                Console.WriteLine("Please enter the string");
+                uInput = Console.ReadLine();
+
+                if (uInput == " ")
+                
+                    break;
+                
+                if (DateTime.TryParse(uInput, out date))
+                {
+                    Console.WriteLine(date.ToString(" , , "));
+                }
+                else
+                {
+                    Console.WriteLine("it is not a date");
+                }
+            }
+            
+            
+        }
+    }
+}
