ece690ac7f1298afe85c634b6306977e1810e998
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2022-11-13 21:45:09 +1100

    Add files via upload

---
+++ DailyPractice10/DailyPractice10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DailyPractice10
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random random = new Random();
+                int a = random.Next(60, 100);
+                Console.WriteLine(a);
+                if (a < 76)
+                {
+                    Console.WriteLine("OK");
+
+                }
+                else if (a > 75 && a < 91)
+                {
+                    Console.WriteLine("Good");
+                }
+                else if (a > 90)
+                {
+                    Console.WriteLine("Excellent");
+                }
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
