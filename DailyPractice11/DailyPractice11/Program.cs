cbd8ac7cb2e5f428e9056d0dc8208fdff63622f0
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2022-11-15 21:30:13 +1100

    Add files via upload

---
+++ DailyPractice11/DailyPractice11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DailyPractice11
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            Random random = new Random();
+           
+            
+
+            while (true)
+            {
+                n = random.Next(100, 999);
+                Console.WriteLine("The number is " + n);
+                int a = n % 10;
+                int b = n % 100/10;
+                int c = (n / 100) % 10;
+                int result = a + b + c;
+                Console.WriteLine("the sum is " + result);
+                Console.ReadKey();
+            }
+            
+
+            
+
+        }
+    }
+}
