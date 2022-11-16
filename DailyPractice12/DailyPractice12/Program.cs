2f6dce1b0e79683e1c157bba7329b4107d84d546
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2022-11-16 21:11:13 +1100

    Add files via upload

---
+++ DailyPractice12/DailyPractice12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DailyPractice12
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num;
+            string a;
+
+            while (true)
+            {
+                a = Console.ReadLine();
+                if (a == " ")
+                {
+                    break;
+                }
+                else
+                {
+                   
+                    num = Convert.ToInt32(a);
+                    Console.WriteLine(num);
+                    if (num < 0)
+                    {
+                        Console.WriteLine("Negative");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Positive");
+                    }
+                }
+                
+            }
+            
+
+            
+            
+        }
+    }
+}
