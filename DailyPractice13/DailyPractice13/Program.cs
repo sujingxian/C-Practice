cc35a5d3330ac0cfa45c877263674efac823fb09
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2022-11-17 22:21:01 +1100

    Add files via upload

---
+++ DailyPractice13/DailyPractice13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DailyPractice13
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number");
+            byte[] arr = new byte[int.Parse(Console.ReadLine())];
+            Random random = new Random();
+            random.NextBytes(arr);
+            foreach(byte i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
