097e21c4e18cb6eef58de2026f3f9a95e2a8a432
Jingxian Su <53254467+sujingxian@users.noreply.github.com>
2022-11-18 22:04:57 +1100

    Add files via upload

---
+++ DailyPractice14/DailyPractice14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DailyPractice14
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                string txt = Console.ReadLine();
+                
+                if(txt == " ")
+                {
+                    break;
+                }
+                Console.WriteLine("The length of the text is " + txt.Length);
+                if (txt.Contains("a"))
+                {
+                    Console.WriteLine("The text has a");
+                }
+                if (txt.Contains("A"))
+                {
+                    Console.WriteLine("The text has A");
+                }
+                else
+                {
+                    txt = txt.Insert(0, "A");
+                    Console.WriteLine("The text is " + txt);
+                }
+            }
+            
+        }
+    }
+}
